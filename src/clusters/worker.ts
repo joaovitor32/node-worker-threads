7f61690fdcc7b01ed8eeaf06d70ca2160d8dc4cb
Joao Vitor Muniz Lopes <joao.lopes@mundiale.com.br>
2023-03-13 22:10:15 -0300

    refactor: change small content

--- src/clusters/worker.ts
+++ src/clusters/worker.ts
@@ -14,9 +14,7 @@ interface ProcessBatchProps {
 
 const processBatch = async ({ initialBatch, index }: ProcessBatchProps) => {
   const chunkSize = batchUtils.getChunkSize(initialBatch);
-
   const batch = batchUtils.sliceChunk({ initialBatch, initialIndex: index, chunkSize });
-
   const promises = await batch.map(async ({ name, url }) => {
     return getPokemonData({ name, url });
   });
@@ -36,14 +34,10 @@ const worker = () => {
 
       const response = await processBatch({ initialBatch, index });
       await cputils.measurement().then((data) => console.log(chalk.cyanBright(data)));
-
       //await setValue({ key: constants.key, value: response });
       await append({ key: constants.key, value: response });
-
       processSize(response);
-
       //Store.PokemonStore.vanillaPokemonStore.getState().store(response);
-
       process.exit(0);
     });
   }
@@ -57,17 +51,13 @@ const triggerWorker = async () => {
 
   Array.from({ length: constants.numCPUs }).forEach((_, index) => {
     const worker = cluster.fork();
-
     console.log(chalk.green(`[${worker.process.pid} - ${worker.id}] Worker process started`));
-
     worker.send({ index, initialBatch });
   });
 
   process.on('beforeExit', async () => {
     const { response } = (await getValue({ key: constants.key })) as { response: { [key: string]: any } };
-
     await fileUtils.writeFile(JSON.stringify(response[constants.key]));
-
     await fileUtils.launchFile(constants.htmlPath);
   });
 
